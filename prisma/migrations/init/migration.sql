0ecee7f1badaf2f216e748284be946abad0ae620
Deepanshu <deepanshusaini2711@gmail.com>
2025-07-06 12:23:21 +0530

    fix: migratign issue primsa

---
+++ prisma/migrations/init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateEnum
+CREATE TYPE "Provider" AS ENUM ('google', 'github');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "image" TEXT,
+    "password" TEXT,
+    "githubId" TEXT,
+    "accessToken" TEXT,
+    "provider" "Provider" NOT NULL DEFAULT 'github',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_githubId_key" ON "users"("githubId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_accessToken_key" ON "users"("accessToken");
+
